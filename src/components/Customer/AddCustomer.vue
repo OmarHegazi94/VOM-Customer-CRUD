075d6e281430e0e343618caf63a10ddfad9e039b
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2023-10-06 20:27:50 +0300

    finished project adding validation

--- src/components/Customer/AddCustomer.vue
+++ src/components/Customer/AddCustomer.vue
@@ -5,6 +5,8 @@ import { db } from '@/firebase/init.js'
 import { collection, addDoc } from 'firebase/firestore'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 const addSuccess = ref(false)
 const router = useRouter()
@@ -18,33 +20,46 @@ const newCustomerForm = reactive({
   avatar: ''
 })
 
-async function addCustomer() {
-  const newCustomer = {
-    name: newCustomerForm.name,
-    email: newCustomerForm.email,
-    phone: newCustomerForm.phone,
-    description: newCustomerForm.description,
-    avatar: newCustomerForm.avatar
-  }
-  console.log('newCustomer', newCustomer)
+const rules = {
+  name: { required },
+  email: { required, email },
+  phone: { required },
+  description: { required },
+  avatar: { required }
+}
 
-  await addDoc(collection(db, 'Customers'), newCustomer)
-    .then(() => {
-      addSuccess.value = true
-      for (const property in newCustomerForm) {
-        newCustomerForm[property] = ''
-      }
-      // console.log('docRef', docRef.id);
-    })
-    .catch((error) => {
-      console.log('Error Adding this document', error)
-    })
-    .finally(() => {
-      setTimeout(() => {
-        addSuccess.value = false
-        router.push({ path: '/' })
-      }, 2000)
-    })
+const v$ = useVuelidate(rules, newCustomerForm)
+
+async function addCustomer() {
+  const result = await v$.value.$validate()
+  if (result) {
+    const newCustomer = {
+      name: newCustomerForm.name,
+      email: newCustomerForm.email,
+      phone: newCustomerForm.phone,
+      description: newCustomerForm.description,
+      avatar: newCustomerForm.avatar
+    }
+    console.log('newCustomer', newCustomer)
+
+    await addDoc(collection(db, 'Customers'), newCustomer)
+      .then(() => {
+        addSuccess.value = true
+        for (const property in newCustomerForm) {
+          newCustomerForm[property] = ''
+        }
+        // console.log('docRef', docRef.id);
+      })
+      .catch((error) => {
+        console.log('Error Adding this document', error)
+      })
+      .finally(() => {
+        setTimeout(() => {
+          addSuccess.value = false
+          router.push({ path: '/' })
+        }, 2000)
+      })
+  }
 }
 </script>
 
@@ -82,6 +97,7 @@ async function addCustomer() {
       </div>
     </div>
     <h3 class="mb-3">Add New Customer</h3>
+    
     <form @submit.prevent="addCustomer" class="needs-validation" novalidate="">
       <div class="row g-3 my-3">
         <div class="col-sm-6">
@@ -89,12 +105,21 @@ async function addCustomer() {
           <input
             type="text"
             class="form-control"
+            :class="{ 'is-invalid': v$.name.$errors.length > 0 }"
             id="name"
             placeholder="John Wick"
             required=""
             v-model="newCustomerForm.name"
           />
-          <div class="invalid-feedback">Valid first name is required.</div>
+          <div v-show="v$.name.$errors" class="div">
+            <div
+              v-for="error in v$.name.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
 
         <div class="col-sm-6">
@@ -102,12 +127,21 @@ async function addCustomer() {
           <input
             type="email"
             class="form-control"
+            :class="{ 'is-invalid': v$.email.$errors.length > 0 }"
             id="email"
             placeholder="johnwick@chapter4.com"
             required=""
             v-model="newCustomerForm.email"
           />
-          <div class="invalid-feedback">Valid Email is required.</div>
+          <div v-show="v$.email.$errors" class="div">
+            <div
+              v-for="error in v$.email.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
       <div class="row g-3 my-3">
@@ -116,12 +150,21 @@ async function addCustomer() {
           <input
             type="text"
             class="form-control"
+            :class="{ 'is-invalid': v$.phone.$errors.length > 0 }"
             id="phone"
             placeholder="99999999"
             required=""
             v-model="newCustomerForm.phone"
           />
-          <div class="invalid-feedback">Valid Phone is required.</div>
+          <div v-show="v$.phone.$errors" class="div">
+            <div
+              v-for="error in v$.phone.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
 
         <div class="col-sm-6">
@@ -129,25 +172,46 @@ async function addCustomer() {
           <input
             type="string"
             class="form-control"
+            :class="{ 'is-invalid': v$.avatar.$errors.length > 0 }"
             id="avatar"
             placeholder="https://picsum.photos/9/200/300"
             required=""
             v-model="newCustomerForm.avatar"
           />
-          <div class="invalid-feedback">Valid Avatar url is required.</div>
+          <div v-show="v$.avatar.$errors" class="div">
+            <div
+              v-for="error in v$.avatar.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
 
       <div class="row g-3 my-3">
         <div class="col-sm-12 mb-5">
           <label class="form-label">Description</label>
-          <QuillEditor v-model:content="newCustomerForm.description" theme="snow" content-type="text" />
-          <div class="invalid-feedback">Valid Description is required.</div>
+          <QuillEditor
+            v-model:content="newCustomerForm.description"
+            theme="snow"
+            content-type="text"
+          />
+          <div v-show="v$.description.$errors" class="div">
+            <div
+              v-for="error in v$.description.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
 
       <!-- <hr class="my-4" /> -->
-      <div class="row g-3 mt-4">
+      <div class="row g-3 mt-5">
         <div class="col-sm-12">
           <button class="w-100 btn btn-primary btn-lg" type="submit">Add Customer</button>
         </div>
--- src/components/Customer/EditCustomer.vue
+++ src/components/Customer/EditCustomer.vue
@@ -4,7 +4,9 @@ import { useRoute, useRouter } from 'vue-router'
 import { db } from '@/firebase/init.js'
 import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 const route = useRoute()
 const router = useRouter()
@@ -37,31 +39,43 @@ const editCustomerForm = reactive({
   avatar: ''
 })
 
+const rules = {
+  name: { required },
+  email: { required, email },
+  phone: { required },
+  description: { required },
+  avatar: { required }
+}
+
+const v$ = useVuelidate(rules, editCustomerForm)
+
 async function editCustomer() {
-  const customer = {
-    name: editCustomerForm.name,
-    email: editCustomerForm.email,
-    phone: editCustomerForm.phone,
-    description: editCustomerForm.description,
-    avatar: editCustomerForm.avatar
-  }
+  const result = await v$.value.$validate()
+  if (result) {
+    const customer = {
+      name: editCustomerForm.name,
+      email: editCustomerForm.email,
+      phone: editCustomerForm.phone,
+      description: editCustomerForm.description,
+      avatar: editCustomerForm.avatar
+    }
 
-  // Update a new document with a generated id.
-  const docRef = doc(db, 'Customers', customerId)
-  await updateDoc(docRef, customer)
-    .then(() => {
-      updateSuccess.value = true
-    })
-    .catch((error) => {
-      console.log('Error updating this document', error)
-    })
-    .finally(() => {
+    // Update a new document with a generated id.
+    const docRef = doc(db, 'Customers', customerId)
+    await updateDoc(docRef, customer)
+      .then(() => {
+        updateSuccess.value = true
+      })
+      .catch((error) => {
+        console.log('Error updating this document', error)
+      })
+      .finally(() => {
         setTimeout(() => {
-            updateSuccess.value = false
-            router.push({ path: '/' })
+          updateSuccess.value = false
+          router.push({ path: '/' })
         }, 2000)
-
-    })
+      })
+  }
 }
 </script>
 
@@ -107,12 +121,21 @@ async function editCustomer() {
           <input
             type="text"
             class="form-control"
+            :class="{ 'is-invalid': v$.name.$errors.length > 0 }"
             id="name"
             placeholder=""
             required=""
             v-model="editCustomerForm.name"
           />
-          <div class="invalid-feedback">Valid first name is required.</div>
+          <div v-show="v$.name.$errors" class="div">
+            <div
+              v-for="error in v$.name.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
 
         <div class="col-sm-6">
@@ -120,12 +143,21 @@ async function editCustomer() {
           <input
             type="email"
             class="form-control"
+            :class="{ 'is-invalid': v$.email.$errors.length > 0 }"
             id="email"
             placeholder=""
             required=""
             v-model="editCustomerForm.email"
           />
-          <div class="invalid-feedback">Valid last name is required.</div>
+          <div v-show="v$.email.$errors" class="div">
+            <div
+              v-for="error in v$.email.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
       <div class="row g-3 my-3">
@@ -134,12 +166,21 @@ async function editCustomer() {
           <input
             type="text"
             class="form-control"
+            :class="{ 'is-invalid': v$.phone.$errors.length > 0 }"
             id="phone"
             placeholder=""
             required=""
             v-model="editCustomerForm.phone"
           />
-          <div class="invalid-feedback">Valid first name is required.</div>
+          <div v-show="v$.phone.$errors" class="div">
+            <div
+              v-for="error in v$.phone.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
 
         <div class="col-sm-6">
@@ -147,20 +188,41 @@ async function editCustomer() {
           <input
             type="string"
             class="form-control"
+            :class="{ 'is-invalid': v$.avatar.$errors.length > 0 }"
             id="avatar"
             placeholder=""
             required=""
             v-model="editCustomerForm.avatar"
           />
-          <div class="invalid-feedback">Valid Avatar url is required.</div>
+          <div v-show="v$.avatar.$errors" class="div">
+            <div
+              v-for="error in v$.avatar.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
 
       <div class="row g-3 my-3">
         <div class="col-sm-12 mb-5">
           <label for="description" class="form-label">Description</label>
-          <QuillEditor v-model:content="editCustomerForm.description" theme="snow" content-type="text" />
-          <div class="invalid-feedback">Valid Description is required.</div>
+          <QuillEditor
+            v-model:content="editCustomerForm.description"
+            theme="snow"
+            content-type="text"
+          />
+          <div v-show="v$.description.$errors" class="div">
+            <div
+              v-for="error in v$.description.$errors"
+              :key="error.$uid"
+              class="invalid-feedback d-block"
+            >
+              {{ error.$message }}
+            </div>
+          </div>
         </div>
       </div>
 
