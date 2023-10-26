82c576e02a0cb6a9c45e1d9b23684183e206783f
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2023-10-26 14:09:23 +0300

    moved keys to .env file

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2jJzzsIhwNRTMvMJ-dBi6rIWNUXvyUys",
+  apiKey: import.meta.env.VITE_APP_API_KEY,
   authDomain: "vom-customers-crud.firebaseapp.com",
   projectId: "vom-customers-crud",
   storageBucket: "vom-customers-crud.appspot.com",
   messagingSenderId: "72531176239",
-  appId: "1:72531176239:web:2c5bb621bd1b4b2636cfda",
-  measurementId: "G-VXJMR4L25M"
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
